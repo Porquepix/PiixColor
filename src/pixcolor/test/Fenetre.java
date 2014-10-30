9d9455a24d9e51137c951149d50df6658a114dfd
Alexis Andrieu <andrieu.alexis@hotmail.com>
2014-10-30 17:50:45 +0100

    Signed-off-by: Alexis Andrieu <andrieu.alexis@hotmail.com>

--- src/pixcolor/test/Fenetre.java
+++ src/pixcolor/test/Fenetre.java
@@ -1,9 +1,13 @@
 package pixcolor.test;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Panel;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
@@ -30,6 +34,10 @@ public class Fenetre extends JFrame {
 		openFileChooser();
 	}
 	
+	/**
+	 * Ouvre un explorateur de fichier
+	 * Si un fichier est sélectionné appelle la fonction pour le sauvegarder dans le programme
+	 */
 	private void openFileChooser() {
 		JFileChooser fc = new JFileChooser();
 		fc.setDialogTitle("Ajouter une image");
@@ -39,7 +47,23 @@ public class Fenetre extends JFrame {
 		
 		int returnVal = fc.showOpenDialog(this);
 		if(returnVal == JFileChooser.APPROVE_OPTION) {
-			System.out.println(fc.getSelectedFile().getName());
+			saveImage(fc.getSelectedFile());
+		}
+	}
+	
+	/**
+	 * Enregistre une image en interne
+	 * 
+	 * @param file image a enregistré
+	 */
+	public void saveImage(File file) {
+		try {
+			BufferedImage i = ImageIO.read(file);
+			ImageIO.write(ImageHelper.imageToBufferedImage(i.getScaledInstance(100, 100, Image.SCALE_SMOOTH)), "png", new File("images/" + file.getName()));
+			System.out.println("Image sauvegardée.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Un probleme est survenue lors de l'importaion de l'image");
 		}
 	}
 
@@ -48,9 +72,9 @@ public class Fenetre extends JFrame {
 	}
 
 	public static void main(String args[]) {
-		//new Fenetre(500, 200, "Test");
+		new Fenetre(500, 200, "Test");
 		
-		Listing.liste("images");
+		//Listing.liste("images");
 	}
 	
 }
