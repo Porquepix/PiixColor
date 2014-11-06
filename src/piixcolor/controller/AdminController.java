1b3ed8a01a41d39e08b5a480b2ae7c43549e2e4d
Alexis Andrieu <andrieu.alexis@hotmail.com>
2014-11-06 18:31:52 +0100

    Ajout de l'enumeration des extensions

--- src/piixcolor/controller/AdminController.java
+++ src/piixcolor/controller/AdminController.java
@@ -1,5 +1,6 @@
 package piixcolor.controller;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -8,7 +9,6 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import piixcolor.model.Model;
-import piixcolor.test.ImageHelper;
 import piixcolor.util.Config;
 
 public class AdminController extends Controller {
@@ -17,12 +17,20 @@ public class AdminController extends Controller {
 		super(m);
 	}
 	
-	public void saveImage(File image) {
+	/**
+	 * Enregistre une image à l'interieur de l'application.
+	 * L'image est préalablement redimensionnée pour gagner de l'espace disque.
+	 * La méthode possède une sécurité pour éviter l'écrasement d'image (dans le cas où le nom de l'image importée est déjà présent). 
+	 * 
+	 * @param image image a enregisté
+	 * @return true si l'image a pu être enregistré, false sinon
+	 */
+	public boolean saveImage(File image) {
 		try {
 			BufferedImage i = ImageIO.read(image);
 			String imageName = image.getName().split("\\.")[0];
 			
-			//systeme pour empecher l'ecrasement d'image
+			//sécurité pour empécher l'écrasement d'image
 			File f = new File("images/" + imageName + ".png");
 			int j = 1;
 			while (f.exists()) {
@@ -30,12 +38,40 @@ public class AdminController extends Controller {
 				j++;
 			}
 			
-			ImageIO.write(ImageHelper.imageToBufferedImage(i.getScaledInstance(Config.IMG_SIZE, Config.IMG_SIZE, Image.SCALE_SMOOTH)), "png", f);
+			ImageIO.write(resizeImage(i, Config.IMG_SIZE, Config.IMG_SIZE), "png", f);
+			
+			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println("Un probleme est survenue lors de l'importaion de l'image");
+			return false;
 		}
 	}
-	
+
+	/**
+	 * Redimensionne une image.
+	 * 
+	 * @param image image a redimensionné
+	 * @param newWidth largeur de l'image redimensionné
+	 * @param newHeight hauteur de l'image redimensionné
+	 * @return  image correctement redimensionné
+	 */
+	private BufferedImage resizeImage(BufferedImage image, int newWidth, int newHeight) {
+		return imageToBufferedImage(image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH));
+	}
+
+	/**
+	 * Convertie une image de la class Image en image de la class BufferedImage.
+	 * 
+	 * @param image image a convertir
+	 * @return image convertie
+	 */
+	private BufferedImage imageToBufferedImage(Image image) {
+		BufferedImage bi = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+		Graphics g = bi.getGraphics();
+		g.drawImage(image, 0, 0, null);
+		g.dispose();
+		return bi;
+	}
 
 }
